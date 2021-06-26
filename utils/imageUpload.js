2bdf8457aee5d7b2d1d17c4a340e7bb76c3877ae
Adão Wellison Domingos de Oliveira <adao.domingos@ituran.com.br>
2021-06-26 15:47:18 -0300

    ajustes na api de upload de imagens

--- utils/imageUpload.js
+++ utils/imageUpload.js
@@ -1,71 +1,18 @@
-const router = require('express').Router()
-const cloudinary = require('cloudinary')
-const auth = require('../middleware/auth')
-const authAdmin = require('../middleware/authAdmin')
-const fs = require('fs')
+export const imageUpload = async (images) => {
+    let imgArr = []
+    for(const item of images){
+        const formData = new FormData()
+        formData.append("file", item)
+        formData.append("ml_default", process.env.CLOUD_UPDATE_PRESET)
+        formData.append("db5gm6hgs", process.env.CLOUD_NAME)
 
-
-// we will upload image on cloudinary
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
-})
-
-// Upload image only admin can use
-router.post('/upload',auth , authAdmin, (req, res) =>{
-    try {
-        if(!req.files || Object.keys(req.files).length === 0)
-            return res.status(400).json({msg: 'No files were uploaded.'})
-        
-        const file = req.files.file;
-        if(file.size > 1024*1024) {
-            removeTmp(file.tempFilePath)
-            return res.status(400).json({msg: "Size too large"})
-        }
-
-        if(file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png'){
-            removeTmp(file.tempFilePath)
-            return res.status(400).json({msg: "File format is incorrect."})
-        }
-
-        cloudinary.v2.uploader.upload(file.tempFilePath, {folder: "test"}, async(err, result)=>{
-            if(err) throw err;
-
-            removeTmp(file.tempFilePath)
-
-            res.json({public_id: result.public_id, url: result.secure_url})
+        const res = await fetch(process.env.CLOUD_API, {
+            method: "POST",
+            body: formData
         })
 
-
-    } catch (err) {
-        return res.status(500).json({msg: err.message})
+        const data = await res.json()
+        imgArr.push({public_id: data.public_id, url: data.secure_url})
     }
-})
-
-// Delete image only admin can use
-router.post('/destroy',auth , authAdmin, (req, res) =>{
-    try {
-        const {public_id} = req.body;
-        if(!public_id) return res.status(400).json({msg: 'No images Selected'})
-
-        cloudinary.v2.uploader.destroy(public_id, async(err, result) =>{
-            if(err) throw err;
-
-            res.json({msg: "Deleted Image"})
-        })
-
-    } catch (err) {
-        return res.status(500).json({msg: err.message})
-    }
-    
-})
-
-
-const removeTmp = (path) =>{
-    fs.unlink(path, err=>{
-        if(err) throw err;
-    })
+    return imgArr;
 }
-
-module.exports = router
