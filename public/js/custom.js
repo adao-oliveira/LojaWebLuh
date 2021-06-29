4cf9110c457f972a5b3c8114a31f4c78ad409995
Adão Wellison Domingos de Oliveira <adao.domingos@ituran.com.br>
2021-06-29 16:44:59 -0300

    mascara de telefone

--- public/js/custom.js
+++ public/js/custom.js
@@ -28,24 +28,40 @@
     }
   });
 
+  // mascara telefone e bloqueio de letras
+
+  $(document).ready(function () {
+
+    var SPMaskBehavior = function (val) {
+      return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 00000-0000';
+    },
+      spOptions = {
+        onkeypress: function (val, e, field, options) {
+          field.mask(SPMaskBehavior.apply({}, arguments), options);
+        }
+      };
+    $("#telefone").mask(SPMaskBehavior, spOptions);
+    if (!((SPMaskBehavior >= "0") && (SPMaskBehavior <= "9")));
+  })
+
 })(jQuery);
 
-  // Exibe ou oculta o botão
+// Exibe ou oculta o botão
 
-  $(document).ready(function() {
-    $(window).on("scroll", function() {
-      if ($(this).scrollTop() > 100) {
-        $("#back-to-top").fadeIn();
-      } else {
-        $("#back-to-top").fadeOut();
-      }
-    });
-    $("#back-to-top").click(function() {
-      $("html, body").animate({
-          scrollTop: 0,
-        },
-        600
-      );
-      return false;
-    });
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 100) {
+      $("#back-to-top").fadeIn();
+    } else {
+      $("#back-to-top").fadeOut();
+    }
   });
+  $("#back-to-top").click(function () {
+    $("html, body").animate({
+      scrollTop: 0,
+    },
+      600
+    );
+    return false;
+  });
+});
